6a993f2f63f3c49ae30917c2defea07dc3c70ffc
Firas hamed <140715379+imcyber0wl@users.noreply.github.com>
2023-10-28 17:26:10 +0400

    Update settings.py

--- zaudio/settings.py
+++ zaudio/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'zaudioconverter',
+    'converter',
     'bootstrap5',
 ]
 
